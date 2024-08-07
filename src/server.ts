e748a9fecc1b7a99150bddb579348cf49aad00a5
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2024-08-06 22:45:09 -0300

    pronto rota de envio de msgs broadcast

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,27 @@ import fastify from "fastify";
 
 const app = fastify();
 
+interface Body{
+    msg: string
+}
+
+app.post("/enviar-msg", async (req, res) => {
+    const { msg } = req.body as Body;
+
+    const aluno = new FireFly({ host: 'http://localhost:5000', namespace: 'default' });
+
+    const message = await aluno.sendBroadcast({
+        header: {
+        },
+        data: [
+            { 
+                value: msg
+            },
+            ],
+        });
+        return { type: 'message', id: message.header.id };
+})
+
 app.listen({
     port: 3333,
     host: '0.0.0.0'
